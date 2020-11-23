f64c0d5842dd35325b014f465ee3e546b69f8dcd
gregordr <greetechprog@gmail.com>
2020-11-23 01:44:14 +0100

    Frontednd

--- frontend/src/PhotoPage.tsx
+++ frontend/src/PhotoPage.tsx
@@ -4,14 +4,16 @@ import React, { useState } from 'react';
 function Photo(props: { id: string, x: number, y: number, click: any, selected: boolean, outZoom: number }) {
     const url = "https://i.imgur.com/" + props.id + ".jpg"
     const padding = props.selected ? 25 : 0
-    return <div style={{ margin: 10, height: props.y, width: props.x, "align-items": "center", "justify-content": "center", display: "flex" }}>
-        <img style={{ transition: "0.05s linear" }}
+    const [vis, setVis] = useState(0)
+    const opacity = props.selected ? 255 : vis
+    return <div onMouseEnter={() => setVis(0.4)} onMouseLeave={() => setVis(0)} style={{ margin: 5, height: props.y, width: props.x, "align-items": "center", "justify-content": "center", display: "flex", background: "#ffffff22", position: "relative" }}>
+        <input checked={props.selected} type="checkbox" id="vehicle1" onClick={props.click} style={{ transition: "0.08s linear", position: "absolute", left: 15, top: 15, height: 20, width: 20, opacity: opacity }} />
+        <a href={url} ><img style={{ transition: "0.05s linear" }}
             src={url}
             alt="new"
             height={props.y - padding}
             width={props.x - padding}
-            onClick={props.click}
-        /></div>;
+        /></a></div>;
 }
 
 export default function PhotoPage(props: {}) {
@@ -26,5 +28,5 @@ export default function PhotoPage(props: {}) {
 
     }
     const makePhoto = (id: string) => <Photo key={id} id={id} x={300} y={300} click={clickHandler(id)} selected={selected.get(id) ?? false} outZoom={0.9} />
-    return (<div>{photos.map((p) => makePhoto(p))}</div>)
+    return (<div style={{ display: "flex", "flex-wrap": "wrap", "margin-top": 100 }}>{photos.map((p) => makePhoto(p))}</div>)
 }
