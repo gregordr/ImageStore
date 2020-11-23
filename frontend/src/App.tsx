64ff663c53cac0e6f210ed674f00ba4439b505d6
gregordr <greetechprog@gmail.com>
2020-11-23 23:11:22 +0100

    top bar done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import SearchBar from 'material-ui-search-bar';
 import React, { useState } from 'react';
 import './App.css';
 import PhotoPage from './PhotoPage'
@@ -6,9 +7,9 @@ import PhotoPage from './PhotoPage'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <PhotoPage />
-      </header>
+      </div>
     </div>
   );
 }
--- frontend/src/PhotoPage.tsx
+++ frontend/src/PhotoPage.tsx
@@ -1,28 +1,46 @@
-import { isAbsolute } from 'path';
 import React, { useState } from 'react';
+import TopBar from './Components/TopBar';
+import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
 
-function Photo(props: { id: string, x: number, y: number, click: any, selected: boolean, outZoom: number }) {
+function Photo(props: any) {
+
     const url = "https://i.imgur.com/" + props.id + ".jpg"
     const padding = props.selected ? 25 : 0
     const [vis, setVis] = useState(0)
     const opacity = props.selected ? 255 : vis
-    const divStyle = { margin: 5, height: props.y, width: props.x, "align-items": "center", "justify-content": "center", display: "flex", background: "#ffffff22", position: "relative" }
-    const boxStyle = { transition: "0.07s linear", position: "absolute", left: 15, top: 15, height: 20, width: 20, opacity: opacity }
 
-    return <div onMouseEnter={() => setVis(0.4)} onMouseLeave={() => setVis(0)} style={divStyle}>
-        <input checked={props.selected} type="checkbox" id="vehicle1" onClick={props.click} style={boxStyle} />
-        <a href={url} >
+    const useStyles = makeStyles({
+        photoDiv: {
+            margin: 5, height: props.y, width: props.x, "align-items": "center", "justify-content": "center", display: "flex", background: "#ffffff22", position: "relative"
+        },
+        photoBox: { transition: "0.07s all  linear", position: "absolute", left: 15, top: 15, height: 20, width: 20, opacity: opacity }
+    });
+
+    const classes = useStyles();
+
+    const onImageClick = () => {
+        if (props.anySelected()) {
+            props.click();
+        } else {
+            console.log("navigate")
+        }
+    }
+
+    return <div className={classes.photoDiv} onMouseEnter={() => setVis(0.4)} onMouseLeave={() => setVis(0)}>
+        <input className={classes.photoBox} checked={props.selected} type="checkbox" id="vehicle1" onClick={props.click} />
+        <div onClick={onImageClick} >
             <img style={{ transition: "0.05s linear" }}
                 src={url}
                 height={props.y - padding}
                 width={props.x - padding}
             />
-        </a>
+        </div>
     </div>;
 }
 
 export default function PhotoPage(props: {}) {
-    const photos = ["LnAraXJ", "5wgb9id", "uJeLOjr", "uJeLOjr", "uJeLOjr", "uJeLOjr", "uJeLOjr", "uJeLOjr", "uJeLOjr", "uJeLOjr"]
+    const [photos, setPhotos] = useState(["LnAraXJ", "5wgb9id", "uJeLOjr", "cBUBtqp", "FR4svrQ", "sd", "as"])
     const map = new Map<string, boolean>();
     photos.map((p) => map.set(p, false))
     const [selected, setSelected] = useState(map);
@@ -32,6 +50,37 @@ export default function PhotoPage(props: {}) {
         setSelected(copy)
 
     }
-    const makePhoto = (id: string) => <Photo key={id} id={id} x={300} y={300} click={clickHandler(id)} selected={selected.get(id) ?? false} outZoom={0.9} />
-    return (<div style={{ display: "flex", "flex-wrap": "wrap", "margin-top": 100 }}>{photos.map((p) => makePhoto(p))}</div>)
+
+    const anySelected = () => {
+        let anySelected = false;
+        selected.forEach((v, _) => anySelected = anySelected || v)
+        return anySelected;
+    }
+
+    const buttonFunctions = {
+        delete: () => {
+            setPhotos(photos.slice().filter(p => !selected.get(p)))
+            buttonFunctions.unselect()
+        },
+        unselect: () => {
+            const copy = new Map(selected)
+            copy.forEach((_, p) => copy.set(p, false))
+            setSelected(copy)
+        },
+        upload: () => {
+            //Nav to upload page
+        },
+        settings: () => {
+            //Nav to settings page
+        },
+    }
+
+    const makePhoto = (id: string) => <Photo key={id} id={id} x={300} y={300} click={clickHandler(id)} selected={selected.get(id) ?? false} outZoom={0.9} anySelected={anySelected} />
+
+    return (
+        <div className="grid-container">
+            <div className="LeftBar"></div>
+            <TopBar anySelected={anySelected} buttonFunctions={buttonFunctions} />
+            <div className="Main-Content">{photos.map((p) => makePhoto(p))}</div>
+        </div>)
 }
