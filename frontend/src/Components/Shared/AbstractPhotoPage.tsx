16bcff6a7f4472f2f6ffc148559662b0f5770fc2
gregordr <greetechprog@gmail.com>
2020-12-07 22:26:51 +0100

    nonmemo

--- frontend/src/Components/Shared/AbstractPhotoPage.tsx
+++ frontend/src/Components/Shared/AbstractPhotoPage.tsx
@@ -17,11 +17,10 @@ const useStyles = makeStyles({
 const Photo = React.memo(function Photo(props: any) {
     const url = "http://localhost:4000/media/" + props.id;
     const padding = props.selected ? 0.9 : 1.0;
-    const [vis, setVis] = useState(false);
+    const [vis, setVis] = useState(0);
+    const opacity = props.anySelected ? 255 : vis;
 
     const classes = useStyles();
-    const onClick = useCallback(() => props.imageClick(props.id), [props.id]);
-    const onClick2 = useCallback(() => props.click(props.id), [props.id]);
     return (
         <div
             className={classes.photoDiv}
@@ -29,21 +28,14 @@ const Photo = React.memo(function Photo(props: any) {
                 height: props.y,
                 width: props.x,
             }}
-            onMouseEnter={() => setVis(true)}
-            onMouseLeave={() => setVis(false)}
+            onMouseEnter={() => setVis(0.4)}
+            onMouseLeave={() => setVis(0)}
         >
-            <div onClick={onClick}>
+            <div onClick={() => props.imageClick(props.id)}>
                 <img alt={props.id} style={{ transition: "0.05s linear", transform: `scale(${padding})` }} src={url} height={props.y} width={props.x} />
             </div>
-            {(vis || props.selected || props.anySelected) && (
-                <input
-                    className={classes.photoBox}
-                    style={{ opacity: vis && !props.selected && !props.anySelected ? 0.4 : 1.0 }}
-                    readOnly={true}
-                    checked={props.selected}
-                    type="checkbox"
-                    onClick={onClick2}
-                />
+            {(vis || props.anySelected) && (
+                <input className={classes.photoBox} style={{ opacity: opacity }} readOnly={true} checked={props.selected} type="checkbox" onClick={() => props.click(props.id)} />
             )}
         </div>
     );
