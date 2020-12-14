e6ea2d48f29b56c04c3ffcf2fafb0d221e29b1fe
Acurisu <acurisu@gmail.com>
2020-12-14 14:10:07 +0100

    [-] remove unused interface

--- frontend/src/Interfaces.tsx
+++ frontend/src/Interfaces.tsx
@@ -1,6 +1,3 @@
-import { Color } from "@material-ui/lab/Alert";
-import { ReactNode } from "react";
-
 export interface AlbumT {
     id: string;
     name: string;
@@ -13,13 +10,4 @@ export interface PhotoT {
     name: string;
     height: number;
     width: number;
-}
-
-export interface Snack {
-    open: boolean;
-    severity: Color;
-    title: string;
-    body: string;
-    action: ReactNode;
-    autoHideDuration: number | null;
 }
