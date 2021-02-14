d444dfc3d92bc6376a3a6918b04a3894720d27a8
Corbett Ferguson <habitual@habitual-system.habitual-media.com>
2021-02-13 23:11:15 -0500

    Implemented rand0m string password for database, and nginx no longer stomps

--- CLI-Install/imagestore-build.sh
+++ CLI-Install/imagestore-build.sh
@@ -27,13 +27,32 @@ cp -r ../frontend /etc/imagestore/
 cp -r ../backend /etc/imagestore/
 
 #Load up the system with the proper configurations
-cp default.conf /etc/nginx/conf.d/default.conf
+
+if (ss -ln | grep ":8080")
+then
+	loopcheck=1
+	read -p "Port conflict detected. Select new port to host ImageStore:  " port
+	while [ $loopcheck -eq 1 ]
+	do
+		if [[ "$port" =~ [a-zA-Z] ]]
+		then
+			echo "Invalid input detected"
+			read -p "Port conflict detected. Select new port to host ImageStore:  " port
+		else 
+			sed -i "s/8080/$port/" default.conf
+			loopcheck=0
+		fi
+	done
+fi
+cp default.conf /etc/nginx/sites-enabled/imagestore.conf
 cp ImageStoreFRONT.service /etc/systemd/system/ImageStoreFRONT.service
 cp ImageStoreBACK.service /etc/systemd/system/ImageStoreBACK.service
 
 #Init an user and the database
-su postgres -c './postgresql.sh'
-echo "PGSTRING=postgres://imagestore:imagestore@localhost:5432/imagestore" > /etc/imagestore/backend/.env
+ranpass=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+
+su postgres -c "./postgresql.sh $ranpass"
+echo "PGSTRING=postgres://imagestore:$ranpass@localhost:5432/imagestore" > /etc/imagestore/backend/.env
 
 #Install node modules
 cd /etc/imagestore/frontend
--- CLI-Install/postgresql.sh
+++ CLI-Install/postgresql.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
- psql -c "CREATE USER imagestore WITH SUPERUSER PASSWORD 'imagestore';"
+ psql -c "CREATE USER imagestore WITH SUPERUSER PASSWORD '$1';"
  psql -c "CREATE DATABASE imagestore;"
 
